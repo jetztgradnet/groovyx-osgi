679ea5911af59b248ec2dab2a57741ef407dc735
Wolfgang Schell <github@jetztgrad.net>
2010-10-15 01:35:02 +0200

    added getStateAsText() for Bundle and BundleContext

--- groovyx-osgi/src/main/groovy/groovyx/osgi/OsgiCategory.groovy
+++ groovyx-osgi/src/main/groovy/groovyx/osgi/OsgiCategory.groovy
@@ -1,5 +1,22 @@
 package groovyx.osgi
 
-class OsgiCategory {
+import org.osgi.framework.Bundle
+import org.osgi.framework.BundleContext
 
+class OsgiCategory {
+	static String getStateAsText(BundleContext bundleContext) {
+		return getStateAsText(bundleContext.bundle)
+	}
+	
+	static String getStateAsText(Bundle bundle) {
+		def state = "UNKNOWN"
+		switch (bundle.state) {
+		case BundleContext.UNINSTALLED: return "UNINSTALLED"
+		case BundleContext.INSTALLED:   return "INSTALLED"  
+		case BundleContext.RESOLVED:    return "RESOLVED"   
+		case BundleContext.STARTING:    return "STARTING"   
+		case BundleContext.STOPPING:    return "STOPPING"   
+		case BundleContext.ACTIVE:      return "ACTIVE"     
+		}
+	} 
 }
