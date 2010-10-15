9d17f64aec4ef377c7a6335415842338a2bca594
Wolfgang Schell <github@jetztgrad.net>
2010-10-15 02:47:21 +0200

    added test for external runtime

--- groovyx-osgi-runtime/src/test/groovy/groovyx/osgi/runtime/OsgiRuntimeTest.groovy
+++ groovyx-osgi-runtime/src/test/groovy/groovyx/osgi/runtime/OsgiRuntimeTest.groovy
@@ -1,22 +1,32 @@
-package groovyx.osgi.runtime;
+package groovyx.osgi.runtime
 
-import org.codehaus.groovy.osgi.runtime.equinox.EquinoxRuntimeFactory;
-import org.codehaus.groovy.osgi.runtime.equinox.EquinoxRuntime;
-import org.codehaus.groovy.osgi.runtime.felix.FelixRuntimeFactory;
-import org.codehaus.groovy.osgi.runtime.felix.FelixRuntime;
-
-import org.osgi.framework.BundleContext;
-
-import org.junit.Before;
-import org.junit.Test;
+import java.io.File
+import org.junit.*
 import static org.junit.Assert.*
+
+import org.osgi.framework.BundleContext
+
+import org.codehaus.groovy.osgi.runtime.equinox.*
+import org.codehaus.groovy.osgi.runtime.external.*
+
 
 public class OsgiRuntimeTest {
 	OsgiRuntimeBuilder builder
+	File tempDir
 	
 	@Before
 	void setUp() {
 		builder = new OsgiRuntimeBuilder()
+		File cwd = new File(System.getProperty('user.dir'))
+		tempDir = new File(cwd, 'dropins')
+		tempDir.mkdirs()
+	}
+	
+	@After
+	void tearDown() {
+		if (tempDir && tempDir.isDirectory()) {
+			tempDir.deleteDir()
+		}
 	}
 	
 	@Test
@@ -45,6 +55,41 @@ public class OsgiRuntimeTest {
 		
 		runtime.stop()
 		assertFalse(runtime.isRunning())
+		
+		context = runtime.bundleContext
+		assertNull(context)
+	}
+	
+	@Test
+	void testExternalRuntime() throws Exception {
+		def tempDir = this.tempDir
+		
+		OsgiRuntime runtime = builder.build {
+			// test alias
+			framework 'external'
+
+			dropinsDir tempDir
+
+			args {
+				resolverLogLevel = "warn"
+			}
+
+			bundle 'mvn:org.apache.felix:org.apache.felix.fileinstall:3.0.2'
+			bundle group: 'org.apache.felix', name:'org.apache.felix.configadmin', version:'1.2.4'
+		}
+		assertNotNull(runtime)
+		assertEquals(ExternalRuntime.class, runtime.class)
+		
+		assertFalse(runtime.canStart())
+		BundleContext context = runtime.start()
+		assertNull(context)
+		assertTrue(runtime.isRunning())
+		
+		// ensure that the bundle files have been installed into dropins folder
+		assertTrue(new File(tempDir, 'org.apache.felix.fileinstall-3.0.2.jar').exists())
+		assertTrue(new File(tempDir, 'org.apache.felix.configadmin-1.2.4.jar').exists())
+		
+		assertFalse(runtime.canStop())
 		
 		context = runtime.bundleContext
 		assertNull(context)
