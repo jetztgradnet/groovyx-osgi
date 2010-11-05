65f5388ddca51f516a39db39f87baa83dbf1f1f1
Wolfgang Schell <github@jetztgrad.net>
2010-11-05 02:04:01 +0100

    - finished implementation - added javadocs

--- groovyx-osgi/src/main/groovy/groovyx/osgi/OsgiCategory.groovy
+++ groovyx-osgi/src/main/groovy/groovyx/osgi/OsgiCategory.groovy
@@ -18,6 +18,7 @@ package groovyx.osgi
 
 import groovy.lang.Closure;
 
+import java.util.Collections;
 import java.util.Map;
 
 import org.osgi.framework.Bundle
@@ -25,7 +26,43 @@ import org.osgi.framework.BundleContext
 import org.osgi.framework.ServiceReference
 import org.osgi.framework.ServiceRegistration
 
+/**
+ * The Osgi Category provides many convenience methods for instances
+ * of {@link Bundle}, {@link BundleContext}, and {@link ServiceRegistration}, 
+ * mainly regarding searching for and working with services.
+ * 
+ * <p>
+ * {@link #findService(BundleContext, Class)} and its variants can be used to 
+ * get services from the OSGi service registry. Search parameters like filters
+ * can be set using a DSL. The return value is an instance of {@link ServiceWrapper},
+ * which wraps the {@link BundleContext} and all found services (or rather their
+ * corresponding {@link ServiceReference}).
+ * </p>
+ * 
+ * <p>
+ * {@link ServiceWrapper} provides some convenient methods to work with the service. 
+ * It transparently handles getting and returning the service instance from the 
+ * {@link ServiceRegistration} and make sure, that no resources are leaked. The
+ * service and optionally the service properties are handed to a user-provided
+ * {@link Closure}, which performs some service-related action.
+ * </p>
+ * 
+ * <p>Example:
+ * <pre>
+ * use(OsgiCategory) {
+ * 	def services = bundleContext.findServices(MyServiceClass, "someprop=42")
+ * 	services.withEachService(callForNullService: true) { service, props ->
+ * 		println "Service properties: " + props
+ * 		service?.doSomething()
+ *	}
+ * }
+ * </pre>
+ * </p>
+ * 
+ * @author Wolfgang Schell
+ */
 class OsgiCategory {
+	
 	static ServiceWrapper findService(BundleContext bundleContext, Closure closure) {
 		new ServiceFinder(bundleContext)
 				.single()
@@ -128,32 +165,178 @@ class OsgiCategory {
 				.find()
 	}
 	
+	/**
+	 * Perform action for a service. If a service is not available, the closure
+	 * will not be called. This behavior can be changed by calling 
+	 * {@link #withService(ServiceReference, BundleContext, Map, Closure)} with 
+	 * option <code>callForNullService</code> set to <code>true</code>.
+	 * 
+	 * <p>
+	 * The provided closure may receive one, two, or three args:
+	 * 
+	 * <ol>
+	 * <li>service instance</li>
+	 * <li>service properties ({@link Map})</li>
+	 * <li>user options ({@link Map})</li>
+	 * </ol>
+	 * </p>
+	 * 
+	 * <p>Example:
+	 * <pre>
+	 * use(OsgiCategory) {
+	 * 	ServiceReference serviceReference = ...
+	 * 	serviceReference.withService(bundleContext) { service, props ->
+	 * 		println "Service properties: " + props
+	 * 		service?.doSomething()
+	 *	}
+	 * }
+	 * </pre>
+	 * </p>
+	 * 
+	 * @param serviceReference references to service
+	 * @param bundleContext	bundle context
+	 * @param closure service action closure
+	 * 
+	 * @return result of service action
+	 */
+	static Object withService(ServiceReference serviceReference, BundleContext bundleContext, Closure closure) {
+		return withService(bundleContext, serviceReference, closure)
+	}
 	
+	/**
+	 * Perform action for a service.
+	 * 
+	 * <p>
+	 * The provided closure may receive one, two, or three args:
+	 * 
+	 * <ol>
+	 * <li>service instance. May be <code>null</code>, if the service is (no longer) available</li>
+	 * <li>service properties ({@link Map})</li>
+	 * <li>user options ({@link Map})</li>
+	 * </ol>
+	 * </p>
+	 * 
+	 * <p>Example:
+	 * <pre>
+	 * use(OsgiCategory) {
+	 * 	ServiceReference serviceReference = ...
+	 * 	serviceReference.withService(bundleContext, callForNullService: true) { service, props ->
+	 * 		println "Service properties: " + props
+	 * 		service?.doSomething()
+	 *	}
+	 * }
+	 * </pre>
+	 * </p>
+	 * 
+	 * @param serviceReference references to service
+	 * @param bundleContext	bundle context
+	 * @param options map of options. See {@link #withEachService(BundleContext, ServiceReference[], Map, Closure)} for supported options
+	 * @param closure service action closure
+	 * 
+	 * @return result of service action
+	 */
 	static Object withService(ServiceReference serviceReference, BundleContext bundleContext, Map options, Closure closure) {
 		return withService(bundleContext, serviceReference, options, closure)
 	}
 	
+	/**
+	 * Perform action for a service. If a service is not available, the closure
+	 * will not be called. This behavior can be changed by calling 
+	 * {@link #withService(BundleContext, ServiceReference, Map, Closure)} with 
+	 * option <code>callForNullService</code> set to <code>true</code>.
+	 * 
+	 * <p>
+	 * The provided closure may receive one, two, or three args:
+	 * 
+	 * <ol>
+	 * <li>service instance</li>
+	 * <li>service properties ({@link Map})</li>
+	 * <li>user options ({@link Map})</li>
+	 * </ol>
+	 * </p>
+	 * 
+	 * <p>Example:
+	 * <pre>
+	 * use(OsgiCategory) {
+	 * 	ServiceReference serviceReference = ...
+	 * 	bundleContext.withService(serviceReference) { service, props ->
+	 * 		println "Service properties: " + props
+	 * 		service?.doSomething()
+	 *	}
+	 * }
+	 * </pre>
+	 * </p>
+	 * 
+	 * @param bundleContext	bundle context
+	 * @param serviceReference references to service
+	 * @param closure service action closure
+	 * 
+	 * @return result of service action
+	 */
+	static Object withService(BundleContext bundleContext, ServiceReference serviceReference, Closure closure) {
+		return withService(bundleContext, serviceReference, Collections.EMPTY_MAP, closure)
+	}
+	
+	/**
+	 * Perform action for a service.
+	 * 
+	 * <p>
+	 * The provided closure may receive one, two, or three args:
+	 * 
+	 * <ol>
+	 * <li>service instance. May be <code>null</code>, if the service is (no longer) available</li>
+	 * <li>service properties ({@link Map})</li>
+	 * <li>user options ({@link Map})</li>
+	 * </ol>
+	 * </p>
+	 * 
+	 * <p>Example:
+	 * <pre>
+	 * use(OsgiCategory) {
+	 * 	ServiceReference serviceReference = ...
+	 * 	bundleContext.withService(serviceReference, callForNullService: true) { service, props ->
+	 * 		println "Service properties: " + props
+	 * 		service?.doSomething()
+	 *	}
+	 * }
+	 * </pre>
+	 * </p>
+	 *
+	 * @param bundleContext	bundle context
+	 * @param serviceReference references to service
+	 * @param options map of options. See {@link #withEachService(BundleContext, ServiceReference[], Map, Closure)} for supported options
+	 * @param closure service action closure
+	 * 
+	 * @return result of service action
+	 */
 	static Object withService(BundleContext bundleContext, ServiceReference serviceReference, Map options, Closure closure) {
 		Object result
+		
+		if (options == null) {
+			options = Collections.EMPTY_MAP
+		}
+		
 		if (serviceReference != null) {
 			// get service
 			Object service = bundleContext.getService(serviceReference);
 			try {
-				// call closure with service [and properties] as arg(s)
-				switch (closure.getMaximumNumberOfParameters()) {
-					case 3:
-						// params: service, service properties, options
-						result = closure.call(service, mapFromServiceProperties(serviceReference), options ?: [:])
-						break
-					case 2:
-						// params: service, service properties
-						result = closure.call(service, mapFromServiceProperties(serviceReference))
-						break
-					case 1:
-					default:
-						// params: service
-						result = closure.call(service)
-						break
+				if ((service != null) || options.callForNullService) {
+					// call closure with service [and properties] as arg(s)
+					switch (closure.getMaximumNumberOfParameters()) {
+						case 3:
+							// params: service, service properties, options
+							result = closure.call(service, mapFromServiceProperties(serviceReference), options ?: [:])
+							break
+						case 2:
+							// params: service, service properties
+							result = closure.call(service, mapFromServiceProperties(serviceReference))
+							break
+						case 1:
+						default:
+							// params: service
+							result = closure.call(service)
+							break
+					}
 				}
 			}
 			finally {
@@ -167,6 +350,181 @@ class OsgiCategory {
 		}
 		
 		return result
+	}
+	
+	/**
+	 * Perform action for each service. If a service is not available, the closure
+	 * will not be called. This behavior can be changed by calling 
+	 * {@link #withEachService(ServiceReference[], BundleContext, Map, Closure)} with 
+	 * option <code>callForNullService</code> set to <code>true</code>. 
+	 * 
+	 * <p>
+	 * The provided closure may receive one, two, or three args:
+	 * 
+	 * <ol>
+	 * <li>service instance</li>
+	 * <li>service properties ({@link Map})</li>
+	 * <li>user options ({@link Map})</li>
+	 * </ol>
+	 * </p>
+	 * 
+	 * <p>Example:
+	 * <pre>
+	 * use(OsgiCategory) {
+	 * 	ServiceReference[] serviceReferences = ...
+	 * 	serviceReferences.withEachService(bundleContext) { service, props ->
+	 * 		println "Service properties: " + props
+	 * 		service?.doSomething()
+	 *	}
+	 * }
+	 * </pre>
+	 * </p>
+	 * 
+	 * @param serviceReferences references to services
+	 * @param bundleContext	bundle context
+	 * @param closure service action closure
+	 * 
+	 * @return list of results of service action
+	 */
+	static List withEachService(ServiceReference[] serviceReferences, BundleContext bundleContext, Closure closure) {
+		return withEachService(bundleContext, serviceReferences, closure)
+	}
+	
+	/**
+	 * Perform action for each service.
+	 *
+	 * <p>
+	 * The provided closure may receive one, two, or three args:
+	 *
+	 * <ol>
+	 * <li>service instance. May be <code>null</code>, if the service is (no longer) available</li>
+	 * <li>service properties ({@link Map})</li>
+	 * <li>user options ({@link Map})</li>
+	 * </ol>
+	 * </p>
+	 * 
+	 * <p>Example:
+	 * <pre>
+	 * use(OsgiCategory) {
+	 * 	ServiceReference[] serviceReferences = ...
+	 * 	serviceReferences.withEachService(bundleContext, callForNullService: true) { service, props ->
+	 * 		println "Service properties: " + props
+	 * 		service?.doSomething()
+	 *	}
+	 * }
+	 * </pre>
+	 * </p>
+	 *
+	 * @param bundleContext	bundle context
+	 * @param serviceReferences references to services
+	 * @param options map of options. See {@link #withEachService(BundleContext, ServiceReference[], Map, Closure)} for supported options
+	 * @param closure service action closure
+	 *
+	 * @return list of results of service action
+	 */
+	static List withEachService(ServiceReference[] serviceReferences, BundleContext bundleContext, Map options, Closure closure) {
+		return withEachService(bundleContext, serviceReferences, options, closure)
+	}
+	
+	/**
+	 * Perform action for each service. If a service is not available, the closure
+	 * will not be called. This behavior can be changed by calling 
+	 * {@link #withEachService(BundleContext, ServiceReference[], Map, Closure)} with 
+	 * option <code>callForNullService</code> set to <code>true</code>.
+	 * 
+	 * <p>
+	 * The provided closure may receive one, two, or three args:
+	 * 
+	 * <ol>
+	 * <li>service instance. May be <code>null</code>, if the service is (no longer) available</li>
+	 * <li>service properties ({@link Map})</li>
+	 * <li>user options ({@link Map})</li>
+	 * </ol>
+	 * </p>
+	 * 
+	 * <p>Example:
+	 * <pre>
+	 * use(OsgiCategory) {
+	 * 	ServiceReference[] serviceReferences = ...
+	 * 	bundleContext.withEachService(serviceReferences) { service, props ->
+	 * 		println "Service properties: " + props
+	 * 		service?.doSomething()
+	 *	}
+	 * }
+	 * </pre>
+	 * </p>
+	 * 
+	 * @param bundleContext	bundle context
+	 * @param serviceReferences references to services
+	 * @param closure service action closure
+	 * 
+	 * @return list of results of service action
+	 */
+	static List withEachService(BundleContext bundleContext, ServiceReference[] serviceReferences, Closure closure) {
+		return withEachService(bundleContext, serviceReferences, Collections.EMPTY_MAP, closure)
+	}
+	
+	/**
+	 * Perform action for each service.
+	 *
+	 * <p>
+	 * The provided closure may receive one, two, or three args:
+	 *
+	 * <ol>
+	 * <li>service instance. May be <code>null</code>, if the service is (no longer) available</li>
+	 * <li>service properties ({@link Map})</li>
+	 * <li>user options ({@link Map})</li>
+	 * </ol>
+	 * </p>
+	 * 
+	 * <p>
+	 * Supported options: TODO
+	 * <ul>
+	 * <li><b>collectNullResults</b>: if <code>true</code>, <code>null</code> 
+	 * 		values returned by the service action are put into the result list. 
+	 * 		By default, <code>null</code> results are skipped.</li>
+	 * <li><b>callForNullService</b>: if <code>true</code>, the service action
+	 * 		closure will be called, if the service reference returns a <code>null</code>
+	 * 		service, e.g. because the service is no longer available. By default,
+	 * 		the service action closure will not be called, if the service resolves
+	 * 		to <code>null</code>.</li>
+	 * </ul>
+	 * </p>
+	 *
+	 * <p>Example:
+	 * <pre>
+	 * use(OsgiCategory) {
+	 * 	ServiceReference[] serviceReferences = ...
+	 * 	bundleContext.withEachService(serviceReferences, callForNullService: true) { service, props ->
+	 * 		println "Service properties: " + props
+	 * 		service?.doSomething()
+	 *	}
+	 * }
+	 * </pre>
+	 * </p>
+	 *
+	 * @param bundleContext	bundle context
+	 * @param serviceReferences references to services
+	 * @param options map of options. See above for supported options
+	 * @param closure service action closure
+	 *
+	 * @return list of results of service action
+	 */
+	static List withEachService(BundleContext bundleContext, ServiceReference[] serviceReferences, Map options, Closure closure) {
+		List results = []
+		
+		if (options == null) {
+			options = Collections.EMPTY_MAP
+		}
+		
+		serviceReferences.each { ServiceReference serviceReference ->
+			def result = withService(bundleContext, serviceReference, options, closure)
+			if (result || options?.collectNullResults) {
+				results << result
+			}
+		}
+		
+		return results
 	}
 	
 	/**
